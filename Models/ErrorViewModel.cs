9b874b30e05c54c357e9859a5fa90fe0740c46a7
Samuel <chousml@gmail.com>
2021-10-05 07:20:13 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MiniMeeting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
